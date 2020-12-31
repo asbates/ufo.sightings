eeb4feee01abafb7d6270e38d8799670d066c8c0
Andrew Bates <andrewbates73@gmail.com>
2020-12-31 11:45:05 -0800

    add baseline data set of UFO sightings

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' UFO sightings in each U.S. state.
+#'
+#' A data set containing the date and state of UFO sightings since 1970.
+#'
+#' @format A data frame with 63496 rows and 2 columns.
+#'
+#' \describe{
+#'   \item{date}{date of sighting}
+#'   \item{state}{state where sighting occured}
+#' }
+#'
+#' @source \url{https://www.kaggle.com/NUFORC/ufo-sightings}
+"sightings"
